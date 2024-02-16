dce7595accd7e02f6e941fa98ff8d6207b8dca1f
Jeff Moser <jeff@bitsalt.com>
2024-02-16 16:20:55 -0500

    Push to github

---
+++ README.md
@@ -0,0 +1,60 @@
+# DeploymentScaler
+
+## Description
+
+This project was created to make upgrading Kubernetes a faster process when 
+applied to large clusters. This will store all current deployments for a 
+configured namespace, then set the replica set count to zero for those
+deployments. Once the Kubernetes update is complete, a subsequent run of
+this script will restore the replica set count for all deployments.
+
+Notes:
+* This only stops deployments. ReplicaSets and StatefulSets are not included.
+* Not tested on Windows.
+
+## Dependencies
+* Python 3 (not tested on < 3.8.10)
+* kubernetes
+* pick
+* The script assumes that .kube/config will be present in the user's home directory. 
+
+(To do: include a requirements.txt file)
+
+```
+pip install kubernetes
+pip install pick
+```
+
+## Using the script
+
+Set the target namespace in main.py inside main(). Open a shell session and
+navigate to the script's directory.
+```
+python ./main.py
+```
+You will see a list of all available contexts from your .kube/config file. Choose
+a context using up/down arrow keys, then press Enter.
+
+Select a process. Options are:
+* stop pods
+* restore pods
+* cancel
+
+If you choose to stop pods, the replica count of all existing deployments will be
+stored in a .json file in the same directory. If the count is zero, the scipt will
+abort. This helps prevent human error (i.e. stopping a second time and recording
+zero for all replica counts). 
+
+## Support
+Tell people where they can go to for help. It can be any combination of an issue tracker, a chat room, an email address, etc.
+
+## Roadmap
+* The namespace is hard-coded in the script. A future version will
+allow selection from available namespaces.
+* ReplicaSets and StatefulSets are not included, as Deployments reduced running pods by 90%, enough to speed up the Kubernetes upgrade. Adding these may be valuable in other situations.
+
+## Contributing
+Reach me by email if you have ideas for additional features: jeff@bitsalt.com.
+
+## License
+Licensed under AGPL 3.0. See the included file.
---
+++ ScalePods.py
@@ -0,0 +1,57 @@
+from kubernetes import client, config
+from pick import pick
+import json
+
+
+class ScalePods:
+    def __init__(self, cluster, namespace):
+        config.load_kube_config()
+        self.cluster = cluster
+        self.namespace = namespace
+        self.filename = "replica_counts_" + cluster + ".json"
+        self.v1 = client.AppsV1Api(api_client=config.new_client_from_config(context=cluster))
+        self.deployments = self.v1.list_namespaced_deployment(namespace)
+
+    def scale_down(self):
+        print("Scaling down")
+
+        for deploy in self.deployments.items:
+            name = deploy.metadata.name
+            self.v1.patch_namespaced_deployment_scale(name, self.namespace, {"spec": {"replicas": 0}})
+            print(f"Scaled down {self.namespace}/{name} to 0 replicas")
+
+    def scale_up(self):
+        print("Scaling up")
+
+        with open(self.filename, "r") as fn:
+            replica_file = fn.read()
+
+        replicas = json.loads(replica_file)
+
+        for deploy in self.deployments.items:
+            name = deploy.metadata.name
+            replica_count = replicas.get(name)
+            self.v1.patch_namespaced_deployment_scale(name, self.namespace, {"spec": {"replicas": replica_count}})
+            print(f"Scaled up {self.namespace}/{name} to {replica_count} replicas")
+
+    # Gather deployment data and store for scaling up later
+    def fetch_replicas(self):
+        print("Fetching replicas")
+        replica_counts = {}
+
+        for deployment in self.deployments.items:
+            name = deployment.metadata.name
+            replicas = deployment.spec.replicas
+            replica_counts[name] = replicas
+
+        deployment_count = len(replica_counts)
+        if deployment_count == 0:
+            process, proc_index = pick(['No', 'Yes'], title="No deployments to store. Continue?", default_index=0)
+            if process == 'No':
+                return False
+        with open(self.filename, "w") as fn:
+            json.dump(replica_counts, fn)
+
+        print(f"Original replica counts stored locally in {self.filename}")
+        return True
+    
